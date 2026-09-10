578b2f0476b5b26f27cf6fbe86f9ab64104f63fc
iichi <ismailichi29@gmail.com>
2026-09-10 14:02:34 +0100

    changing the jenkins file 1

--- https-enbale.sh
+++ https-enbale.sh
@@ -4,10 +4,10 @@ mkdir -p ~/.config/docker
 
 cat > ~/.config/docker/daemon.json <<EOF
 {
-  "insecure-registries": ["nexus:8091"]
+  "insecure-registries": ["localhost:8091"]
 }
 EOF
 
 systemctl --user restart docker
 
-echo "Done. Docker now accepts HTTP registry: nexus:8091"
+echo "Done. Docker now accepts HTTP registry: localhost:8091"
